0dfae70e187a56403137a9f318156f82c209a4f3
Startsev Alexey <sa@webpractik.ru>
2019-01-08 23:18:07 +0300

    Создание заготовок для визуальной части.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import ControlPanel from "components/ControlPanel/ControlPanel";
+import { controlPanelEntity } from "entities/controlPanel";
+import DaysList from "components/DaysList/DaysList";
+
+let ControlPanelParams = new controlPanelEntity();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <ControlPanel/>
+          <ControlPanel filterParams={ControlPanelParams}/>
+          <DaysList startDate={new Date(Date.now())} daysCount={ControlPanelParams.days} />
       </div>
     );
   }
