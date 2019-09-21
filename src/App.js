4f052fd4d6c5a5c3e40062dff59b00463ac7ab11
sa@webpractik.ru <gaussne4>
2019-09-21 18:03:11 +0300

    Новый план : Создание функционала.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import ControlPanel from "components/ControlPanel/ControlPanel";
+import ControlPanel from 'components/ControlPanel/ControlPanel';
 import { controlPanelEntity } from "entities/controlPanel";
-import CalendarController from "components/CalendarController/CalendarController";
 import { PlanItemCollectionMobx } from "entities/planItems";
-import { getMonday } from "services/helper";
 import ModalWindow from "components/ModalWindow/ModalWindow";
 import { ModalItemEntity } from 'entities/ui';
 import CreatePlanItem from 'components/Forms/CreatePlanItem/CreatePlanItem';
 import appParams from 'startData/app.json';
 import { observer } from "mobx-react";
-import GetData from 'services/GetData';
-import ByJSON from 'dataSource/ByJSON';
 import {CalendarApiDistributor} from 'entities/calendarApiDistributor';
+import EventsOperationsLogic from 'components/EventsOperationsLogic/EventsOperationsLogic';
+import EventModel from 'RestModels/EventModel';
 
 
 let ControlPanelParams = new controlPanelEntity();
@@ -26,14 +24,16 @@ export let appUi = React.createContext({modal: ModalStatus});
 
 const App = observer(class AppClass extends Component {
 
+
     componentWillMount() {
+        const Events = new EventModel();
 
-        let DataSource = new ByJSON('planItems');
-        let FetchData = new GetData(DataSource);
-
-        FetchData.get().then((result) => {
-            this.setState({items: result.items});
+        Events.getAll(ControlPanelParams.user, 20).then((result) => {
+            this.setState({items: result.items}, () => {
+                window.dispatchEvent(new Event('resize'));
+            });
         });
+
     }
 
     state = {
@@ -46,70 +46,22 @@ const App = observer(class AppClass extends Component {
       <div className="App">
           <ControlPanel filterParams={ControlPanelParams}/>
               <appUi.Provider value={{modal: ModalStatus}}>
-                  <div className={'flex'}>
-                      <div className={'flex-container'}>
-                          <CalendarController
-                              planItems={this.state.items}
-                              daysCount={ControlPanelParams.days}
-                              calendarName={'productive_dev1'}
-                              hours={5}
-                              showHeader
-                          />
-                      </div>
-                      <div className={'flex-container'}>
-                          <CalendarController
-                              planItems={this.state.items}
-                              daysCount={ControlPanelParams.days}
-                              calendarName={'productive_dev2'}
-                              hours={5}
-                              showHeader
-                          />
-                      </div>
-                  </div>
-                  <h3>Оценки</h3>
-                  <div className={'flex'}>
-                      <div className={'flex-container'}>
-                          <CalendarController
-                              planItems={this.state.items}
-                              daysCount={ControlPanelParams.days}
-                              calendarName={'estimate_dev1'}
-                              hours={2}
-                          />
-                      </div>
-                      <div className={'flex-container'}>
-                          <CalendarController
-                              planItems={this.state.items}
-                              daysCount={ControlPanelParams.days}
-                              calendarName={'estimate_dev2'}
-                              hours={2}
-                          />
-                      </div>
-                  </div>
-                  <h3>Тех.долг</h3>
-                  <div className={'flex'}>
-                      <div className={'flex-container'}>
-                          <CalendarController
-                              planItems={this.state.items}
-                              daysCount={ControlPanelParams.days}
-                              calendarName={'techrent_dev1'}
-                              hours={2}
-                          />
-                      </div>
-                      <div className={'flex-container'}>
-                          <CalendarController
-                              planItems={this.state.items}
-                              daysCount={ControlPanelParams.days}
-                              calendarName={'techrent_dev2'}
-                              hours={2}
-                          />
-                      </div>
-                  </div>
+                  {
+                      this.state.items.length &&
+                      <EventsOperationsLogic
+                          items={this.state.items}
+                          daysCount={ControlPanelParams.days}
+                          modalControl={ModalStatus}
+                      />
+                  }
               </appUi.Provider>
           <ModalWindow status={ModalStatus}>
               <CreatePlanItem
                   planItemsCollection={PlanItemsCollection}
                   cancel={() => {ModalStatus.makeInvisible()}}
                   params={ModalStatus.params}
+                  successCallback={ModalStatus.successCallback}
+                  cancelCallback={ModalStatus.cancelCallback}
               />
           </ModalWindow>
       </div>
