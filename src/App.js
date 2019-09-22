5fe28e23ac461ac4dc825c7727e9973286065792
Startsev Alexey <sa@webpractik.ru>
2019-09-22 22:17:48 +0300

    Новый план. Отправка события с изменением при перетаскивании между зонами и юзерами.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import ControlPanel from 'components/ControlPanel/ControlPanel';
-import { controlPanelEntity } from "entities/controlPanel";
 import { PlanItemCollectionMobx } from "entities/planItems";
 import ModalWindow from "components/ModalWindow/ModalWindow";
 import { ModalItemEntity } from 'entities/ui';
@@ -11,24 +10,21 @@ import { observer } from "mobx-react";
 import {CalendarApiDistributor} from 'entities/calendarApiDistributor';
 import EventsOperationsLogic from 'components/EventsOperationsLogic/EventsOperationsLogic';
 import EventModel from 'RestModels/EventModel';
+import {zoneList} from 'config/app.config';
 
 
-let ControlPanelParams = new controlPanelEntity();
 let PlanItemsCollection = new PlanItemCollectionMobx();
+
 let ModalStatus = new ModalItemEntity();
-
 ModalStatus.setParams(appParams);
-
 export let appUi = React.createContext({modal: ModalStatus});
 
 
 const App = observer(class AppClass extends Component {
-
-
     componentWillMount() {
         const Events = new EventModel();
 
-        Events.getAll(ControlPanelParams.user, 20).then((result) => {
+        Events.getAll(this.props.controlPanelParams.user, 20).then((result) => {
             this.setState({items: result.items}, () => {
                 window.dispatchEvent(new Event('resize'));
             });
@@ -38,22 +34,58 @@ const App = observer(class AppClass extends Component {
 
     state = {
         items: [],
+        items2: [],
+        splitMode: false,
         apiCollection: CalendarApiDistributor.getInstance()
     };
 
-  render() {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('fdfs')
+
+        const {controlPanelParams} = this.props;
+        if (controlPanelParams.splitCalendar && !this.state.items2.length) {
+            const Events = new EventModel();
+
+            Events.getAll(controlPanelParams.user2, 20).then((result) => {
+                this.setState({items2: result.items, splitMode: true}, () => {
+                    //window.dispatchEvent(new Event('resize'));
+                });
+            });
+        }
+    }
+
+    render() {
     return (
       <div className="App">
-          <ControlPanel filterParams={ControlPanelParams}/>
+          <ControlPanel filterParams={this.props.controlPanelParams}/>
               <appUi.Provider value={{modal: ModalStatus}}>
+                  <div className="flex">
                   {
                       this.state.items.length &&
-                      <EventsOperationsLogic
-                          items={this.state.items}
-                          daysCount={ControlPanelParams.days}
-                          modalControl={ModalStatus}
-                      />
+                          <div className="flex-container">
+                              <EventsOperationsLogic
+                                  userId={this.props.controlPanelParams.user}
+                                  zones={zoneList}
+                                  items={this.state.items}
+                                  daysCount={this.props.controlPanelParams.days}
+                                  modalControl={ModalStatus}
+                              />
+                          </div>
                   }
+
+                  {
+                      (this.state.items2.length && this.props.controlPanelParams.splitCalendar) &&
+                      <div className="flex-container">
+                          <EventsOperationsLogic
+                              userId={this.props.controlPanelParams.user2}
+                              zones={zoneList}
+                              items={this.state.items2}
+                              daysCount={this.props.controlPanelParams.days}
+                              modalControl={ModalStatus}
+                          />
+                      </div>
+                  }
+                  </div>
               </appUi.Provider>
           <ModalWindow status={ModalStatus}>
               <CreatePlanItem
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {controlPanelEntity} from "entities/controlPanel";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let ControlPanelParams = new controlPanelEntity();
+ReactDOM.render(<App controlPanelParams={ControlPanelParams}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
