2a5cc502e6d02f8e23b8dd212f961a79eeb1db79
Startsev Alexey <sa@webpractik.ru>
2019-09-26 10:17:42 +0300

    Новый план : Добавлены уведомления, модель темы. Запрос и обновление данных при смене юзера в панели.

--- src/App.js
+++ src/App.js
@@ -1,106 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import ControlPanel from 'components/ControlPanel/ControlPanel';
-import { PlanItemCollectionMobx } from "entities/planItems";
-import ModalWindow from "components/ModalWindow/ModalWindow";
-import { ModalItemEntity } from 'entities/ui';
-import CreatePlanItem from 'components/Forms/CreatePlanItem/CreatePlanItem';
-import appParams from 'startData/app.json';
-import { observer } from "mobx-react";
-import {CalendarApiDistributor} from 'entities/calendarApiDistributor';
-import EventsOperationsLogic from 'components/EventsOperationsLogic/EventsOperationsLogic';
-import EventModel from 'RestModels/EventModel';
-import {zoneList} from 'config/app.config';
+import MainComponent from 'components/MainComponent/MainComponent';
+import withThemeParams from 'components/HOC/withThemeParams';
+import withNotifies from 'components/HOC/withNotifies';
+import SnackNotifier from 'components/Notifiers/SnackNotifier';
 
 
-let PlanItemsCollection = new PlanItemCollectionMobx();
-
-let ModalStatus = new ModalItemEntity();
-ModalStatus.setParams(appParams);
-export let appUi = React.createContext({modal: ModalStatus});
-
-
-const App = observer(class AppClass extends Component {
-    componentWillMount() {
-        const Events = new EventModel();
-
-        Events.getAll(this.props.controlPanelParams.user, 20).then((result) => {
-            this.setState({items: result.items}, () => {
-                window.dispatchEvent(new Event('resize'));
-                this.forceUpdate()
-            });
-        });
-
-    }
-
-    state = {
-        items: [],
-        items2: [],
-        splitMode: false,
-        apiCollection: CalendarApiDistributor.getInstance()
-    };
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-
-        const {controlPanelParams} = this.props;
-        if (controlPanelParams.splitCalendar && !this.state.items2.length) {
-            const Events = new EventModel();
-
-            Events.getAll(controlPanelParams.user2, 20).then((result) => {
-                this.setState({items2: result.items}, () => {
-                    //window.dispatchEvent(new Event('resize'));
-                });
-            });
-        }
-    }
+class App extends Component {
 
     render() {
-    const {controlPanelParams} = this.props;
+
+    const PlanComponent = withNotifies(MainComponent, SnackNotifier);
 
     return (
       <div className="App">
-          <ControlPanel filterParams={controlPanelParams}/>
-              <appUi.Provider value={{modal: ModalStatus}}>
-                  <div className="flex">
-                  {
-                      this.state.items.length &&
-                          <div className="flex-container">
-                              <EventsOperationsLogic
-                                  userId={controlPanelParams.user}
-                                  zones={zoneList}
-                                  items={this.state.items}
-                                  daysCount={+controlPanelParams.days}
-                                  modalControl={ModalStatus}
-                              />
-                          </div>
-                  }
-
-                  {
-                      (controlPanelParams.splitCalendar) &&
-                      <div className="flex-container">
-                          <EventsOperationsLogic
-                              userId={controlPanelParams.user2}
-                              zones={zoneList}
-                              items={this.state.items2}
-                              daysCount={+controlPanelParams.days}
-                              modalControl={ModalStatus}
-                          />
-                      </div>
-                  }
-                  </div>
-              </appUi.Provider>
-          <ModalWindow status={ModalStatus}>
-              <CreatePlanItem
-                  planItemsCollection={PlanItemsCollection}
-                  cancel={() => {ModalStatus.makeInvisible()}}
-                  params={ModalStatus.params}
-                  successCallback={ModalStatus.successCallback}
-                  cancelCallback={ModalStatus.cancelCallback}
-              />
-          </ModalWindow>
+          <PlanComponent controlPanelParams={this.props.controlPanelParams}/>
       </div>
     );
   }
-});
+}
 
 export default App;
