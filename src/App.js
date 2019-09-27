ca66052fcdaa6c9e04ff57eba9f769e78242676e
Startsev Alexey <sa@webpractik.ru>
2019-09-27 10:15:09 +0300

    Новый план : Небольшая чистка после рефакторинга.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainComponent from 'components/MainComponent/MainComponent';
-import withThemeParams from 'components/HOC/withThemeParams';
 import withNotifies from 'components/HOC/withNotifies';
 import SnackNotifier from 'components/Notifiers/SnackNotifier';
 
